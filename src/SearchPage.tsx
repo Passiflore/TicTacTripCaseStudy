013d291a1d7e535ffd98d69a165f475d88fe0124
Passiflore <elise.vue@hetic.net>
2023-04-25 03:29:17 +0200

    Adding new card and animation

--- src/SearchPage.tsx
+++ src/SearchPage.tsx
@@ -1,7 +1,24 @@
 import './SearchPage.css'
 import Header from './components/Hearder'
+import React, {useEffect, useState} from 'react'
+import { Link, useLocation, useParams } from 'react-router-dom';
 
 function SearchPage() {
+    const location = useLocation();
+    console.log(location.state.arrival)
+    const [town, setTown] = useState<any[]>([]);
+
+    useEffect(() =>{
+        // if (searchWord !== "") {
+        fetch(`https://api.comparatrip.eu/cities/popular/from/${location.state.arrival}/5`)
+            .then(response => response.json())
+            .then(data => setTown(data))
+            .catch(error => console.error(error));
+        // } else {
+        //     setTown([]);
+        // }
+    }, [location.state.arrival])
+    console.log(town)
 
     return (
         <>
@@ -13,12 +30,29 @@ function SearchPage() {
                         Départ :
                         <input placeholder="D'où partons-nous ?"></input>
                     </div>
-                    <div className='searchPageInput departureInput'>
+                    <div className='searchPageInput arrivalInput'>
                         Arrivée :
                         <input placeholder="Où allons nous ?">
                         </input>
+                        {location.state.arrival}
                     </div>
                 </div>
+                {town.length > 0 && (
+                    <ul>
+                    {town.map(t => (
+                        <li key={t.unique_name}>
+                            <Link 
+                                to=
+                                    '/searchPage'
+                                state = {{
+                                    arrival: t.unique_name
+                            }}>
+                                <a>{t.local_name}</a>
+                            </Link>
+                        </li>
+                    ))}
+                    </ul>
+                )}
                 
             </body>
         </>
